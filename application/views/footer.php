426ca77cc4d9991a7df1d0d52985a258bc02b561
norlihazmey <norlihazmey89@gmail.com>
2015-01-29 23:43:03 +0800

    manage header, footer, header_top and menus in their own file

---
+++ application/views/footer.php
@@ -0,0 +1,16 @@
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/raphael-min.js"></script>
+    <!--<script src="<?php echo base_url();?>assets/js/morris.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/morris-data.js"></script>-->
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/sb-admin-2.js"></script>
